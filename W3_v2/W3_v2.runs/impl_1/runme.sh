04fe058901fadd5640e46f8fae19c0251c0df04f
BertRamlot <bert.ramlot@gmail.com>
2021-10-21 17:30:01 +0200

    H3 done

--- W3_v2/W3_v2.runs/impl_1/runme.sh
+++ W3_v2/W3_v2.runs/impl_1/runme.sh
@@ -41,7 +41,7 @@ EAStep()
 }
 
 # pre-commands:
-/bin/touch .write_bitstream.begin.rst
+/bin/touch .init_design.begin.rst
 EAStep vivado -log multi_segment.vdi -applog -m64 -product Vivado -messageDb vivado.pb -mode batch -source multi_segment.tcl -notrace
 
 
